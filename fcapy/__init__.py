72afdb1bc602c73544499e06eb516ca0c18580dc
Egor Dudyrev <egor.dudyrev@yandex.ru>
2020-12-01 22:37:45 +0300

    Drop checking dependency on json lib. As it is standard library

--- fcapy/__init__.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-try:
-    import json
-    del json
-except ImportError:
-    msg = 'The package `json` is not install. Thus all the functions to read/write json files will raise ImportError' +\
-        "Run `pip install json` to solve the problem"
-    warnings.warn(msg)
