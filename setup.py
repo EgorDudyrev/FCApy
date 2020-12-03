4954feef9cb0402b09fb0f158ff309e9fb5ed123
Egor <egor.dudyrev@yandex.ru>
2020-12-04 01:19:53 +0300

    Revert "Develop"

--- setup.py
+++ setup.py
@@ -1,36 +1,22 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def run_install(**kwargs):
-    with open("README.md", "r") as fh:
-        long_description = fh.read()
-
-    extras_require = {
-        'context': [
-            'pandas'
-        ],
-    }
-    extras_require['all'] = list(set(i for val in extras_require.values() for i in val))
-
-    setuptools.setup(
-        name="fcapy",
-        version="0.0.5",
-        author="Egor Dudyrev",
-        author_email="egor.dudyrev@yandex.ru",
-        description="A library to work with formal (and pattern) contexts, concepts, lattices",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/EgorDudyrev/FCApy",
-        packages=setuptools.find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.0',
-        extras_require=extras_require
+setuptools.setup(
+    name="fcapy",
+    version="0.0.5",
+    author="Egor Dudyrev",
+    author_email="egor.dudyrev@yandex.ru",
+    description="A library to work with formal (and pattern) contexts, concepts, lattices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EgorDudyrev/FCApy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
 )
-
-
-if __name__ == "__main__":
-    run_install()
