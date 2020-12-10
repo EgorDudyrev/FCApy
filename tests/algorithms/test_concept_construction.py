fd44df8bc07abd12ca9ec6de8f4b7fb7a3ccaa76
Egor Dudyrev <egor.dudyrev@yandex.ru>
2020-12-10 21:59:34 +0300

    Move algorithms to specified subpackage

--- tests/algorithms/test_concept_construction.py
+++ tests/algorithms/test_concept_construction.py
@@ -1,6 +1,6 @@
 import json
 from fcapy.context import read_json
-from fcapy.lattice import concept_construction_algorithms
+from fcapy.algorithms import concept_construction
 from fcapy.lattice.formal_concept import FormalConcept
 
 
@@ -10,6 +10,6 @@ def test_close_by_one():
     concepts_loaded = {FormalConcept.from_dict(c_json) for c_json in file_data}
 
     context = read_json("data/animal_movement.json")
-    concepts_constructed = concept_construction_algorithms.close_by_one(context)
+    concepts_constructed = concept_construction.close_by_one(context)
     assert set(concepts_constructed) == concepts_loaded,\
         "Close_by_one error. Constructed concepts do not match the true ones"
---
+++ tests/algorithms/test_lattice_construction.py
@@ -0,0 +1,2 @@
+import pytest
+from fcapy.algorithms import lattice_construction as lca
