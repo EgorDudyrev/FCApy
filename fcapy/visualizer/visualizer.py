3803112302fc4bfaeb24938815e7fe783088b88d
Egor Dudyrev <egor.dudyrev@yandex.ru>
2020-12-15 21:12:10 +0300

    Set networkx.multipartite_layout for concept lattice nodes position

--- fcapy/visualizer/visualizer.py
+++ fcapy/visualizer/visualizer.py
@@ -1,29 +1,37 @@
+import networkx as nx
+
+
 class Visualizer:
-    def __init__(self, lattice):
+    def __init__(self, lattice=None):
         self._lattice = lattice
-        self._pos = self.get_nodes_position()
+        self._pos = self.get_nodes_position() if lattice is not None else None
 
-    def get_nodes_position(self):
-        ltc = self._lattice
-        levels = [0] * len(ltc.concepts)
-        nodes_to_visit = [ltc.top_concept_i]
-        while len(nodes_to_visit) > 0:
-            node_id = nodes_to_visit.pop(0)
-            spc_ids = ltc.superconcepts_dict[node_id]
-            levels[node_id] = max([levels[spc_id] for spc_id in spc_ids]) + 1 if len(spc_ids) > 0 else 0
-            nodes_to_visit += ltc.subconcepts_dict[node_id]
-
-        levels_dict = {i: [] for i in range(len(levels))}
-        for c_i, c in enumerate(ltc.concepts):
-            levels_dict[levels[c_i]].append(c_i)
-
-        pos = {}
-        for l_i, c_is in levels_dict.items():
-            for idx, c_i in enumerate(c_is):
-                pos[c_i] = (idx, -l_i)
+    def get_nodes_position(self, lattice=None):
+        lattice = self._lattice if lattice is None else lattice
+        c_levels, levels_dict = self._calc_levels()
+        n_levels = len(levels_dict)
+        digraph = nx.DiGraph(lattice.subconcepts_dict)
+        for c_i, l_i in enumerate(c_levels):
+            digraph.nodes[c_i]['level'] = l_i
+        pos = nx.multipartite_layout(digraph, subset_key='level', align='horizontal')
+        pos = {c_i: [p[0], -p[1]] for c_i, p in pos.items()}
         return pos
 
+    def _calc_levels(self, lattice=None):
+        lattice = self._lattice if lattice is None else lattice
+        c_levels = [0] * len(lattice.concepts)
+        nodes_to_visit = [lattice.top_concept_i]
+        while len(nodes_to_visit) > 0:
+            node_id = nodes_to_visit.pop(0)
+            spc_ids = lattice.superconcepts_dict[node_id]
+            c_levels[node_id] = max([c_levels[spc_id] for spc_id in spc_ids]) + 1 if len(spc_ids) > 0 else 0
+            nodes_to_visit += lattice.subconcepts_dict[node_id]
+
+        levels_dict = {i: [] for i in range(max(c_levels) + 1)}
+        for c_i, c in enumerate(lattice.concepts):
+            levels_dict[c_levels[c_i]].append(c_i)
+        return c_levels, levels_dict
+
     def draw_networkx(self):
-        import networkx as nx
         graph = nx.from_dict_of_lists(self._lattice.subconcepts_dict)
         nx.draw_networkx(graph, self._pos)
