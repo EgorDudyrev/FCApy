47c848516dca3b3b1e92472abded0893c3497bd7
Egor Dudyrev <egor.dudyrev@yandex.ru>
2021-03-23 13:56:37 +0300

    Add docstrings for algorithms subpackage

---
+++ fcapy/algorithms/__init__.py
@@ -0,0 +1,17 @@
+"""
+This subpackage contains all the complex algorithms of the package.
+
+Modules:
+--------
+  concept_construction:
+    This module contains a number of functions which take a FormalContext (or MVContext)
+    and return a set of formal (or pattern) concepts.
+    Some of them return a ConceptLattice instead of just a set of concepts
+  lattice_construction:
+    This module contains a number of function which take a set of formal (or pattern) concepts
+    and return its subconcepts_dict
+    i.e. the order of given concepts in the form {parent_concept_index: child_concept_index}.
+    Parent_concept is a concept which is bigger (or more general) than the child concept
+    and there is no other concept between these two.
+
+"""
