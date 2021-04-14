918a8aad61413a783a202c9cc757ab2c94271e45
Egor Dudyrev <egor.dudyrev@yandex.ru>
2021-04-14 19:08:32 +0300

    Add basic cache functionality for poset (almost)

--- fcapy/poset/poset.py
+++ fcapy/poset/poset.py
@@ -10,9 +10,16 @@ from collections.abc import Collection
 
 
 class POSet:
-    def __init__(self, elements=None, leq_func=None):
+    def __init__(self, elements=None, leq_func=None, use_cache: bool = True):
         self._elements = list(elements) if elements is not None else None
         self._leq_func = leq_func
+
+        self._use_cache = use_cache
+        if self._use_cache:
+            self._cache = {i: {} for i in range(len(elements))} if elements is not None else None
+            self.leq_elements = self._leq_elements_cache
+        else:
+            self.leq_elements = self._leq_elements_nocache
 
     @property
     def elements(self):
@@ -25,13 +32,13 @@ class POSet:
     def super_elements(self, element_index: int):
         el = self._elements[element_index]
         sup_indexes = [i for i, el_comp in enumerate(self._elements)
-                       if self._leq_func(el, el_comp) and i != element_index]
+                       if self.leq_elements(element_index, i) and i != element_index]
         return sup_indexes
 
     def sub_elements(self, element_index: int):
         el = self._elements[element_index]
         sub_indexes = [i for i, el_comp in enumerate(self._elements)
-                       if self._leq_func(el_comp, el) and i != element_index]
+                       if self.leq_elements(i, element_index) and i != element_index]
         return sub_indexes
 
     def join_elements(self, element_indexes: Collection = None):
@@ -71,19 +78,63 @@ class POSet:
         return self.meet_elements(element_indexes)
 
     def leq_elements(self, a_index: int, b_index: int):
+        """Placeholder to use instead of either self._leq_elements_nocache(...) or self._leq_elements_cache(...)"""
+        raise NotImplementedError
+
+    def _leq_elements_nocache(self, a_index: int, b_index: int):
         return self._leq_func(self._elements[a_index], self._elements[b_index])
+
+    def _leq_elements_cache(self, a_index: int, b_index: int):
+        res = self._cache[a_index].get(b_index)
+        if res is None:
+            res = self._leq_func(self._elements[a_index], self._elements[b_index])
+            self._cache[a_index][b_index] = res
+        return res
 
     def __getitem__(self, item: int or slice or Collection):
         return slice_list(self._elements, item) if not isinstance(item, int) else self._elements[item]
 
     def __and__(self, other):
-        assert self._leq_func == other.leq_func,\
+        """Placeholder to use instead of either self._and_nocache(...) or self._and_cache(...)"""
+        if self._use_cache:
+            return self._and_cache(other)
+        else:
+            return self._and_nocache(other)
+
+    def _and_nocache(self, other):
+        assert self._leq_func == other.leq_func, \
+            'POSet.__and__ assertion. Compared posets have to have the same leq_func'
+        elements_and = [x for x in self._elements if x in other._elements]
+        s = POSet(elements_and, self._leq_func)
+        return s
+
+    def _and_cache(self, other):
+        #TODO: Rewrite to use cache
+
+        assert self._leq_func == other.leq_func, \
             'POSet.__and__ assertion. Compared posets have to have the same leq_func'
         elements_and = [x for x in self._elements if x in other._elements]
         s = POSet(elements_and, self._leq_func)
         return s
 
     def __or__(self, other):
+        """Placeholder to use instead of either self._or_nocache(...) or self._or_cache(...)"""
+        if self._use_cache:
+            return self._or_cache(other)
+        else:
+            return self._or_nocache(other)
+
+    def _or_nocache(self, other):
+        assert self._leq_func == other.leq_func, \
+            'POSet.__or__ assertion. Compared posets have to have the same leq_func'
+        elements_or = [x for x in self._elements]
+        elements_or += [x for x in other._elements if x not in elements_or]
+        s = POSet(elements_or, self._leq_func)
+        return s
+
+    def _or_cache(self, other):
+        #TODO: Rewrite to use cache
+
         assert self._leq_func == other.leq_func, \
             'POSet.__or__ assertion. Compared posets have to have the same leq_func'
         elements_or = [x for x in self._elements]
@@ -92,6 +143,23 @@ class POSet:
         return s
 
     def __xor__(self, other):
+        """Placeholder to use instead of either self._xor_nocache(...) or self._xor_cache(...)"""
+        if self._use_cache:
+            return self._xor_cache(other)
+        else:
+            return self._xor_nocache(other)
+
+    def _xor_nocache(self, other):
+        assert self._leq_func == other.leq_func, \
+            'POSet.__xor__ assertion. Compared posets have to have the same leq_func'
+        elements_or = [x for x in self._elements if x not in other._elements]
+        elements_or += [x for x in other._elements if x not in self._elements]
+        s = POSet(elements_or, self._leq_func)
+        return s
+
+    def _xor_cache(self, other):
+        #TODO: Rewrite to use cache
+
         assert self._leq_func == other.leq_func, \
             'POSet.__xor__ assertion. Compared posets have to have the same leq_func'
         elements_or = [x for x in self._elements if x not in other._elements]
@@ -113,3 +181,12 @@ class POSet:
 
     def __eq__(self, other):
         return self._leq_func == other._leq_func and self._elements == other._elements
+
+    def fill_up_cache(self):
+        assert self._use_cache,\
+            "POSet.fill_up_cache assertion. The cache can only be filled up if it is enabled (`POSet.use_cache = True`)"
+
+        for i in range(len(self)):
+            for j in range(len(self)):
+                if j not in self._cache[i]:
+                    self.leq_elements(i, j)
--- tests/poset/test_poset.py
+++ tests/poset/test_poset.py
@@ -1,4 +1,5 @@
 from fcapy.poset import POSet
+import pytest
 
 
 def test_init():
@@ -6,26 +7,52 @@ def test_init():
 
     elements = ['', 'a', 'b', 'ab']
     leq_func = lambda x,y: x in y
+
+    for use_cache in [False, True]:
+        s = POSet(elements, leq_func, use_cache=use_cache)
+        assert s._elements == elements
+        assert s._leq_func == leq_func
+        assert s._use_cache == use_cache
+
     s = POSet(elements, leq_func)
-    assert s._elements == elements
-    assert s._leq_func == leq_func
+    assert s._use_cache
+    assert s._cache
 
 
 def test_leq_elements():
     elements = ['', 'a', 'b', 'ab']
     leq_func = lambda x, y: x in y
-    s = POSet(elements, leq_func)
-    leq = s.leq_elements(1, 2)
-    assert not leq
+    for use_cache in [False, True]:
+        s = POSet(elements, leq_func, use_cache=use_cache)
+        leq = s.leq_elements(1, 2)
+        assert not leq
 
-    leq = s.leq_elements(0, 2)
-    assert leq
+        leq = s.leq_elements(0, 2)
+        assert leq
 
-    leq = s.leq_elements(2, 0)
-    assert not leq
+        leq = s.leq_elements(2, 0)
+        assert not leq
 
-    leq = s.leq_elements(2, 2)
-    assert leq
+        leq = s.leq_elements(2, 2)
+        assert leq
+
+
+def test_fill_up_cache():
+    elements = ['', 'a', 'b', 'ab']
+    leq_func = lambda x, y: x in y
+    s = POSet(elements, leq_func, use_cache=False)
+    with pytest.raises(AssertionError):
+        s.fill_up_cache()
+
+    s = POSet(elements, leq_func, use_cache=True)
+    s.fill_up_cache()
+    cache_true = {
+        0: {0: True, 1: True, 2: True, 3: True},
+        1: {0: False, 1: True, 2: False, 3: True},
+        2: {0: False, 1: False, 2: True, 3: True},
+        3: {0: False, 1: False, 2: False, 3: True}
+    }
+    assert s._cache == cache_true
 
 
 def test_join_elements_supremum():
@@ -102,29 +129,90 @@ def test_eq():
 def test_and():
     elements = ['', 'a', 'b', 'ab']
     leq_func = lambda x, y: x in y
-    s1 = POSet(elements[:-1], leq_func)
-    s2 = POSet(elements[1:], leq_func)
-    s_and = POSet(elements[1:-1], leq_func)
-    assert s1 & s2 == s_and
+
+    # Test if intersection operation is working
+    for use_cache in [False, True]:
+        s1 = POSet(elements[:-1], leq_func, use_cache=use_cache)
+        s2 = POSet(elements[1:], leq_func, use_cache=use_cache)
+        s_and_true = POSet(elements[1:-1], leq_func, use_cache=use_cache)
+        s_and_fact = s1 & s2
+        assert s_and_fact == s_and_true
+
+    # Test if cache of intersection is intersection of filled up caches
+    s1 = POSet(elements[:-1], leq_func, use_cache=True)
+    s1.fill_up_cache()
+    s2 = POSet(elements[1:], leq_func, use_cache=True)
+    s2.fill_up_cache()
+    s_and_true = POSet(elements[1:-1], leq_func)
+    s_and_true.fill_up_cache()
+
+    s_and_fact = s1 & s2
+    assert s_and_fact == s_and_true
+    assert s_and_fact._cache == s_and_true._cache
+
+    # Test if cache of intersection is union of caches
+    s1 = POSet(elements, leq_func, use_cache=True)
+    s1.leq_elements(0, 1)
+    s2 = POSet(elements[:-1], leq_func, use_cache=True)
+    s2.leq_elements(1, 2)
+    s_and_true = POSet(elements[:-1], leq_func)
+    s_and_true.leq_elements(0,1)
+    s_and_true.leq_elements(1,2)
+
+    s_and_fact = s1 & s2
+    assert s_and_fact == s_and_true
+    assert s_and_fact._cache == s_and_true._cache
 
 
 def test_or():
     elements = ['', 'a', 'b', 'ab']
     leq_func = lambda x, y: x in y
-    s1 = POSet(elements[:-1], leq_func)
-    s2 = POSet(elements[1:], leq_func)
-    s_or = POSet(elements, leq_func)
-    assert s1 | s2 == s_or
+
+    # Test if union operation is working
+    for use_cache in [False, True]:
+        s1 = POSet(elements[:-1], leq_func, use_cache=use_cache)
+        s2 = POSet(elements[1:], leq_func, use_cache=use_cache)
+        s_or_true = POSet(elements, leq_func, use_cache=use_cache)
+        s_or_fact = s1 | s2
+        assert s_or_fact == s_or_true
+
+    # Test if cache of union is union of filled up caches
+    s1 = POSet(elements[:-1], leq_func, use_cache=True)
+    s1.fill_up_cache()
+    s2 = POSet(elements[1:], leq_func, use_cache=True)
+    s2.fill_up_cache()
+    s_or_true = POSet(elements, leq_func, use_cache=True)
+    s_or_true.fill_up_cache()
+
+    s_or_fact = s1 | s2
+    assert s_or_fact == s_or_true
+    assert s_or_fact._cache == s_or_true._cache
 
 
 def test_xor():
     elements = ['', 'a', 'b', 'ab']
     elements_xor = ['', 'ab']
     leq_func = lambda x, y: x in y
-    s1 = POSet(elements[:-1], leq_func)
-    s2 = POSet(elements[1:], leq_func)
-    s_xor = POSet(elements_xor, leq_func)
-    assert s1 ^ s2 == s_xor
+
+    # Test if xor operation is working
+    for use_cache in [False, True]:
+        s1 = POSet(elements[:-1], leq_func, use_cache=use_cache)
+        s2 = POSet(elements[1:], leq_func, use_cache=use_cache)
+        s_xor_true = POSet(elements_xor, leq_func, use_cache=use_cache)
+        s_xor_fact = s1 ^ s2
+        assert s_xor_fact == s_xor_true
+
+    # Test if cache of xor is xor of filled up caches
+    s1 = POSet(elements[:-1], leq_func, use_cache=True)
+    s1.fill_up_cache()
+    s2 = POSet(elements[1:], leq_func, use_cache=True)
+    s2.fill_up_cache()
+    s_xor_true = POSet(elements_xor, leq_func, use_cache=True)
+    s_xor_true.fill_up_cache()
+
+    s_xor_fact = s1 ^ s2
+    assert s_xor_fact == s_xor_true
+    assert s_xor_fact._cache == s_xor_true._cache
 
 
 def test_len():
@@ -143,6 +231,8 @@ def test_delitem():
     del s[del_i]
     assert s.elements == elements_del
 
+    #TODO: Add test of caches
+
 
 def test_add():
     elements = ['', 'a', 'b', 'ab']
@@ -151,6 +241,8 @@ def test_add():
     s.add(elements[-1])
     assert s.elements == elements
 
+    #TODO: Add test of caches
+
 
 def test_remove():
     elements = ['', 'a', 'b', 'ab']
@@ -158,6 +250,8 @@ def test_remove():
     s = POSet(elements, leq_func)
     s.remove(elements[-1])
     assert s.elements == elements[:-1]
+
+    #TODO: Add test of caches
 
 
 def test_super_elements():
