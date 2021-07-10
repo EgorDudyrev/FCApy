863005189776087d989a2c1174981d20195d967b
Azam Bekhruz <bekhruz.azam@kaspersky.com>
2021-07-10 05:45:24 +0500

    Lindig algo optimization

--- tests/algorithms/test_concept_construction.py
+++ tests/algorithms/test_concept_construction.py
@@ -182,4 +182,23 @@ def test_lindig_algorithm():
     L_lin = cca.lindig_algorithm(context)
     L_cbo = ConceptLattice.from_context(context, algo='CbO')
     
-    assert L_lin == L_cbo, 'lindig_algorithm lattice mismatch'
+    assert len(L_lin.concepts) == len(L_cbo.concepts), 'lindig_algorithm concepts len mismatch'
+    
+    foo = {}
+
+    for i, x in enumerate(L_lin.concepts):
+        for j, y in enumerate(L_cbo.concepts):
+            if (x.extent_i, x.intent_i) == (y.extent_i, y.intent_i):
+                foo[i] = j
+                
+    assert len(foo) == len(L_cbo.concepts), 'lindig_algorithm concepts mismatch'
+    
+    for i in range(len(L_lin.concepts)):
+        lin = sorted([foo[x] for x in L_lin._cache_direct_subelements.get(i, [])])
+        cbo = sorted([x for x in L_cbo._cache_direct_subelements.get(foo[i], [])])
+        assert lin == cbo, 'lindig_algorithm lattice mismatch'
+        
+    for i in range(len(L_lin.concepts)):
+        lin = sorted([foo[x] for x in L_lin._cache_direct_superelements.get(i, [])])
+        cbo = sorted([x for x in L_cbo._cache_direct_superelements.get(foo[i], [])])
+        assert lin == cbo, 'lindig_algorithm lattice mismatch'
