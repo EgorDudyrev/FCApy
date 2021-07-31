59b5e275bb2f2c374f044db95711ea855ff5f924
Egor Dudyrev <egor.dudyrev@yandex.ru>
2021-07-31 17:39:08 +0400

    Add test for calc_levels function. Prettify tests for layouts

--- tests/visualizer/test_layouts.py
+++ tests/visualizer/test_layouts.py
@@ -5,6 +5,22 @@ from fcapy.visualizer import layouts
 import numpy as np
 
 
+def test_calc_levels():
+    path = 'data/animal_movement.json'
+    K = FormalContext.read_json(path)
+
+    K_df = K.to_pandas()
+    for m in K.attribute_names:
+        K_df[f'not_{m}'] = ~K_df[m]
+    K = FormalContext.from_pandas(K_df)
+
+    L = ConceptLattice.from_context(K)
+
+    c_levels, levels_dict = layouts.calc_levels(L)
+    assert all([c_levels[c_i] < c_levels[sub_i] for c_i, subs_i in L.sub_elements_dict.items() for sub_i in subs_i]),\
+        'Calc_levels function failed. Some elements have level not bigger than that of their superelements '
+
+
 def test_multipartite_layout():
     path = 'data/animal_movement.json'
     K = FormalContext.read_json(path)
@@ -12,23 +28,12 @@ def test_multipartite_layout():
 
     pos_fact = layouts.multipartite_layout(L)
     pos_true = {
-        0: [0.0, 1.0],
-        1: [0.6666666666666666, 0.3333333333333333],
-        2: [0.0, 0.3333333333333333],
-        3: [-0.6666666666666666, 0.3333333333333333],
-        4: [0.6666666666666666, -0.3333333333333333],
-        5: [0.0, -0.3333333333333333],
-        6: [-0.6666666666666666, -0.3333333333333333],
-        7: [0.0, -1.0]
+        0: [0.0000, 1.0000], 1: [0.6667, 0.3333], 2: [0.0000, 0.3333], 3: [-0.6667, 0.3333],
+        4: [0.6667, -0.3333], 5: [0.0000, -0.3333], 6: [-0.6667, -0.3333], 7: [0.0000, -1.0000]
     }
-    pos_diff_dict = {
-        c_i: np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum())
-        for c_i in range(len(L))
-    }
-    pos_diff_mean = np.mean(list(pos_diff_dict.values()))
-    assert pos_diff_mean < 1e-6, \
-        f'layouts.multipartite_layout failed. Nodes position calculated wrongly.' + \
-        f'Position differences: {pos_diff_dict}'
+    pos_diff = [np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum()) for c_i in range(len(L))]
+    assert max(pos_diff) < 1e-4, \
+        f'layouts.multipartite_layout failed. Nodes position calculated wrongly.'
 
 
 def test_fcart_layout():
@@ -37,30 +42,30 @@ def test_fcart_layout():
     L = ConceptLattice.from_context(K)
 
     pos_fact = layouts.fcart_layout(L)
-    
-    pos_true = {0: [0.0, 1.0], 1: [-0.7142857142857143, 0.6666666666666667], 2: [-0.4285714285714286, 0.6666666666666667], 3: [-0.1428571428571429, 0.6666666666666667], 4: [0.1428571428571428, 0.6666666666666667], 5: [-0.75, 0.33333333333333337], 6: [-0.5, 0.33333333333333337], 7: [0.4285714285714286, 0.6666666666666667], 8: [0.0, 0.33333333333333337], 9: [0.25, 0.33333333333333337], 10: [-0.6666666666666667, 0.0], 11: [-0.33333333333333337, 0.0], 12: [-0.25, 0.33333333333333337], 13: [0.5, 0.33333333333333337], 14: [0.7142857142857142, 0.6666666666666667], 15: [0.33333333333333326, 0.0], 16: [0.33333333333333326, -0.33333333333333326], 17: [0.6666666666666667, 0.0], 18: [0.0, 0.0], 19: [0.75, 0.33333333333333337], 20: [-0.33333333333333337, -0.33333333333333326], 21: [0.0, -0.6666666666666667]}
-    
-    pos_diff_dict = {
-        c_i: np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum())
-        for c_i in range(len(L))
+
+    pos_true = {
+        0: [0.0000, 1.0000], 1: [-0.7143, 0.6667], 2: [-0.4286, 0.6667], 3: [-0.1429, 0.6667], 4: [0.1429, 0.6667],
+        5: [-0.7500, 0.3333], 6: [-0.5000, 0.3333], 7: [0.4286, 0.6667], 8: [0.0000, 0.3333], 9: [0.2500, 0.3333],
+        10: [-0.6667, 0.0000], 11: [-0.3333, 0.0000], 12: [-0.2500, 0.3333], 13: [0.5000, 0.3333], 14: [0.7143, 0.6667],
+        15: [0.3333, 0.0000], 16: [0.3333, -0.3333], 17: [0.6667, 0.0000], 18: [0.0000, 0.0000], 19: [0.7500, 0.3333],
+        20: [-0.3333, -0.3333], 21: [0.0000, -0.6667]
     }
-    pos_diff_mean = np.mean(list(pos_diff_dict.values()))
-    assert pos_diff_mean < 1e-6, \
-        f'layouts.fcart_layout failed. Nodes position calculated wrongly.' + \
-        f'Position differences: {pos_diff_dict}'
+    pos_diff = [np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum()) for c_i in range(len(L))]
+    assert max(pos_diff) < 1e-4, \
+        f'layouts.fcart_layout failed. Nodes position calculated wrongly.'
     
     pos_fact = layouts.fcart_layout(L, c=1, dpth=2)
-    
-    pos_true = {0: [0.0, 1.0], 1: [-0.7142857142857143, 0.6666666666666667], 2: [-0.4285714285714286, 0.6666666666666667], 3: [-0.1428571428571429, 0.6666666666666667], 4: [0.1428571428571428, 0.6666666666666667], 5: [-0.75, 0.33333333333333337], 6: [-0.5, 0.33333333333333337], 7: [0.4285714285714286, 0.6666666666666667], 8: [0.0, 0.33333333333333337], 9: [0.25, 0.33333333333333337], 10: [0.0, 0.0], 11: [-0.6666666666666667, 0.0], 12: [-0.25, 0.33333333333333337], 13: [0.5, 0.33333333333333337], 14: [0.7142857142857142, 0.6666666666666667], 15: [-0.33333333333333337, 0.0], 16: [-0.33333333333333337, -0.33333333333333326], 17: [0.33333333333333326, 0.0], 18: [0.6666666666666667, 0.0], 19: [0.75, 0.33333333333333337], 20: [0.33333333333333326, -0.33333333333333326], 21: [0.0, -0.6666666666666667]}
-    
-    pos_diff_dict = {
-        c_i: np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum())
-        for c_i in range(len(L))
+
+    pos_true = {
+        0: [0.0000, 1.0000], 1: [-0.7143, 0.6667], 2: [-0.4286, 0.6667], 3: [-0.1429, 0.6667], 4: [0.1429, 0.6667],
+        5: [-0.7500, 0.3333], 6: [-0.5000, 0.3333], 7: [0.4286, 0.6667], 8: [0.0000, 0.3333], 9: [0.2500, 0.3333],
+        10: [0.0000, 0.0000], 11: [-0.6667, 0.0000], 12: [-0.2500, 0.3333], 13: [0.5000, 0.3333], 14: [0.7143, 0.6667],
+        15: [-0.3333, 0.0000], 16: [-0.3333, -0.3333], 17: [0.3333, 0.0000], 18: [0.6667, 0.0000], 19: [0.7500, 0.3333],
+        20: [0.3333, -0.3333], 21: [0.0000, -0.6667]
     }
-    pos_diff_mean = np.mean(list(pos_diff_dict.values()))
-    assert pos_diff_mean < 1e-6, \
-            f'layouts.fcart _layout failed. Nodes position calculated wrongly while changing parameters.' + \
-        f'Position differences: {pos_diff_dict}'
+    pos_diff = [np.sqrt(((np.array(pos_fact[c_i]) - np.array(pos_true[c_i])) ** 2).sum()) for c_i in range(len(L))]
+    assert max(pos_diff) < 1e-4, \
+        f'layouts.fcart _layout failed. Nodes position calculated wrongly while changing parameters.'
 
 
 def test_layouts_dict():
