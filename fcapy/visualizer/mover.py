7f429065b1bf95669bbffedbe0b4adbd3cdd8b91
Egor Dudyrev <egor.dudyrev@yandex.ru>
2021-11-14 22:28:00 +0300

    Add function to swap nodes

--- fcapy/visualizer/mover.py
+++ fcapy/visualizer/mover.py
@@ -10,6 +10,20 @@ PosDictType = Dict[int, Tuple[float, float]]
 
 
 @dataclass
+class DifferentHierarchyLevelsError(ValueError):
+    el_a: int
+    el_b: int
+    operation_name: str = ''
+
+    def __str__(self):
+        msg = '\n'.join([
+            f"Elements {self.el_a} and {self.el_b} are on the different hierarchy levels.",
+            f"Thus the operation{' '+self.operation_name} cannot be performed",
+        ])
+        return msg
+
+
+@dataclass
 class Mover:
     pos: PosDictType = None
 
@@ -23,3 +37,9 @@ class Mover:
         layout_func = LAYOUTS[layout]
         kwargs_used = get_kwargs_used(kwargs, layout_func)
         self.pos = layout_func(poset, **kwargs_used)
+
+    def swap_nodes(self, el_a: int, el_b: int) -> None:
+        if self.pos[el_a][1] != self.pos[el_b][1]:
+            raise DifferentHierarchyLevelsError(el_a, el_b, "'swap_nodes'")
+
+        self.pos[el_a], self.pos[el_b] = self.pos[el_b], self.pos[el_a]
--- tests/visualizer/test_mover.py
+++ tests/visualizer/test_mover.py
@@ -1,4 +1,4 @@
-from fcapy.visualizer.mover import Mover
+from fcapy.visualizer.mover import Mover, DifferentHierarchyLevelsError
 from fcapy.visualizer import hasse_layouts
 
 from fcapy.context import FormalContext
@@ -10,6 +10,13 @@ import pytest
 def test_init():
     mvr = Mover()
     assert mvr.pos is None
+
+    pos = {
+        0: (0.0, 1.0), 1: (-0.5, 0.5), 2: (0.0, 0.5), 3: (0.5, 0.5),
+        4: (-0.5, 0.0), 5: (0.0, 0.0), 6: (0.5, 0.0), 7: (0.0, -0.5)
+    }
+    mvr = Mover(pos=pos)
+    assert pos == pos
 
 
 def test_get_nodes_position():
@@ -23,3 +30,17 @@ def test_get_nodes_position():
 
     with pytest.raises(ValueError):
         mvr.initialize_pos(L, 'FaKeLaYoUt')
+
+
+def test_swap_nodes():
+    pos = {
+        0: (0.0, 1.0), 1: (-0.5, 0.5), 2: (0.0, 0.5), 3: (0.5, 0.5),
+        4: (-0.5, 0.0), 5: (0.0, 0.0), 6: (0.5, 0.0), 7: (0.0, -0.5)
+    }
+    mvr = Mover(pos={k: v for k, v in pos.items()})
+
+    mvr.swap_nodes(1, 2)
+    assert mvr.pos[1] == pos[2] and mvr.pos[2] == pos[1]
+
+    with pytest.raises(DifferentHierarchyLevelsError):
+        mvr.swap_nodes(1, 4)
