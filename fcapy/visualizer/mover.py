8adb7ba4867b4e2c2f45df108119aef327250733
Egor Dudyrev <egor.dudyrev@yandex.ru>
2021-11-21 18:39:11 +0300

    Make Mover work with levels and peers coordinates instead of pure pos dict

--- fcapy/visualizer/mover.py
+++ fcapy/visualizer/mover.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List, Optional
 
 from attr import dataclass
 
@@ -23,12 +23,69 @@ class DifferentHierarchyLevelsError(ValueError):
         return msg
 
 
-@dataclass
 class Mover:
-    pos: PosDictType = None
+    """Class to make node moving in Hasse Diagrams easier
+
+    Methods
+    -------
+    pos: get/set nodes position in {element_idx: (x_coord, y_coord)} format
+
+    Attributes
+    ----------
+    levels: Position of each node in level hierarchy (0 = Top element, 1,2,3,... lower levels)
+    peers_order: Position of each node inside a level (i = The i-th element)
+
+    pos_levels: Float coordinate of each level
+    pos_peers: Float coordinate of each peer by level
+    """
+    levels: Optional[List[Optional[int]]]
+    peers_order: Optional[List[Optional[int]]]
+
+    pos_levels: Optional[List[float]]
+    pos_peers: Optional[List[List[float]]]
+
+    def __init__(self, pos: PosDictType = None):
+        self.pos = pos
+
+    @property
+    def pos(self) -> Optional[PosDictType]:
+        if self.levels is None:
+            return None
+
+        return {el_i: (self.pos_peers[lvl][peer], self.pos_levels[lvl])
+                for el_i, (lvl, peer) in enumerate(zip(self.levels, self.peers_order))}
+
+    @pos.setter
+    def pos(self, value: PosDictType):
+        if value is None:
+            self.levels = None
+            self.peers_order = None
+            self.pos_levels = None
+            self.pos_peers = None
+            return
+
+        max_el_i = max(value)
+        lvl_coords = sorted(set([value[el_i][1] if el_i in value else None for el_i in range(max_el_i + 1)]),
+                            reverse=True)
+        lvl_coords_inv_dct = {coord: lvl_i for lvl_i, coord in enumerate(lvl_coords)}
+        levels = [lvl_coords_inv_dct[value[el_i][1]] if el_i in value else None for el_i in range(max_el_i + 1)]
+
+        peers_order = [[] for _ in range(len(lvl_coords))]
+        for el_i, lvl in enumerate(levels):
+            peers_order[lvl].append(el_i)
+        peers_order = [sorted(peers, key=lambda el_i: value[el_i][0]) for peers in peers_order]
+        pos_peers = [[value[el_i][0] for el_i in peers] for peers in peers_order]
+
+        peers_order_flat = [peers_order[lvl].index(el_i) if lvl is not None else None
+                            for el_i, lvl in enumerate(levels)]
+
+        self.levels = levels
+        self.peers_order = peers_order_flat
+        self.pos_levels = lvl_coords
+        self.pos_peers = pos_peers
 
     def initialize_pos(self, poset: POSet, layout='fcart', **kwargs) -> None:
-        """Return a dict of nodes positions in a line diagram"""
+        """Return a dict of nodes float positions in a line diagram"""
         if layout not in LAYOUTS:
             raise ValueError(
                 f'Layout "{layout}" is not supported. '
@@ -39,7 +96,8 @@ class Mover:
         self.pos = layout_func(poset, **kwargs_used)
 
     def swap_nodes(self, el_a: int, el_b: int) -> None:
-        if self.pos[el_a][1] != self.pos[el_b][1]:
+        lvl_a, lvl_b = [self.levels[el] for el in [el_a, el_b]]
+        if lvl_a is None or lvl_b is None or lvl_a != lvl_b:
             raise DifferentHierarchyLevelsError(el_a, el_b, "'swap_nodes'")
 
-        self.pos[el_a], self.pos[el_b] = self.pos[el_b], self.pos[el_a]
+        self.peers_order[el_a], self.peers_order[el_b] = self.peers_order[el_b], self.peers_order[el_a]
